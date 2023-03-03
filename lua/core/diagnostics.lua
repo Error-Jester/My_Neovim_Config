d42e891e4bef5611345ec59a9a0c8e72ef6083b2
Duah Kwadwo Adjei <duah14@outlook.com>
2023-03-03 12:51:30 +0000

    Another config

--- lua/core/diagnostics.lua
+++ lua/core/diagnostics.lua
@@ -1,14 +1,14 @@
 local signs = {
-  { name = "DiagnosticSignError", text = astronvim.get_icon "DiagnosticError" },
-  { name = "DiagnosticSignWarn", text = astronvim.get_icon "DiagnosticWarn" },
-  { name = "DiagnosticSignHint", text = astronvim.get_icon "DiagnosticHint" },
-  { name = "DiagnosticSignInfo", text = astronvim.get_icon "DiagnosticInfo" },
-  { name = "DiagnosticSignError", text = astronvim.get_icon "DiagnosticError" },
-  { name = "DapStopped", text = astronvim.get_icon "DapStopped", texthl = "DiagnosticWarn" },
-  { name = "DapBreakpoint", text = astronvim.get_icon "DapBreakpoint", texthl = "DiagnosticInfo" },
-  { name = "DapBreakpointRejected", text = astronvim.get_icon "DapBreakpointRejected", texthl = "DiagnosticError" },
+  { name = "DiagnosticSignError",    text = astronvim.get_icon "DiagnosticError" },
+  { name = "DiagnosticSignWarn",     text = astronvim.get_icon "DiagnosticWarn" },
+  { name = "DiagnosticSignHint",     text = astronvim.get_icon "DiagnosticHint" },
+  { name = "DiagnosticSignInfo",     text = astronvim.get_icon "DiagnosticInfo" },
+  { name = "DiagnosticSignError",    text = astronvim.get_icon "DiagnosticError" },
+  { name = "DapStopped",             text = astronvim.get_icon "DapStopped",             texthl = "DiagnosticWarn" },
+  { name = "DapBreakpoint",          text = astronvim.get_icon "DapBreakpoint",          texthl = "DiagnosticInfo" },
+  { name = "DapBreakpointRejected",  text = astronvim.get_icon "DapBreakpointRejected",  texthl = "DiagnosticError" },
   { name = "DapBreakpointCondition", text = astronvim.get_icon "DapBreakpointCondition", texthl = "DiagnosticInfo" },
-  { name = "DapLogPoint", text = astronvim.get_icon "DapLogPoint", texthl = "DiagnosticInfo" },
+  { name = "DapLogPoint",            text = astronvim.get_icon "DapLogPoint",            texthl = "DiagnosticInfo" },
 }
 
 for _, sign in ipairs(signs) do
@@ -18,10 +18,10 @@ end
 
 astronvim.lsp.diagnostics = {
   off = {
-    underline = false,
-    virtual_text = false,
-    signs = false,
-    update_in_insert = false,
+    underline = true,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = true,
   },
   on = astronvim.user_plugin_opts("diagnostics", {
     virtual_text = true,
